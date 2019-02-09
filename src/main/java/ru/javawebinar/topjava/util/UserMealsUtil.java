20258c8c1caa9a8e0cf668837a289a01efddcf79
Alex <alex.kulga@gmail.com>
2019-02-09 10:50:50 +0300

    Training of commits from IDE

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -26,6 +26,8 @@ public class UserMealsUtil {
 
     public static List<UserMealWithExceed>  getFilteredWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
         // TODO return filtered list with correctly exceeded field
+        System.out.println("TODO List with correct exceeded field");
+      //  mealList.stream().filter(p->TimeUtil.isBetween(p.getDateTime().toLocalTime(),startTime,endTime));
         return null;
     }
 }
